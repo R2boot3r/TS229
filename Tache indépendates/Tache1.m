d90e14f875ff5919f95b6fb29d243063f3103afd
agnia <aabulaeva@enseirb-matmeca.fr>
2019-11-02 12:36:03 +0100

    tache 1 fonctionelle

--- Tache indépendates/Tache1.m
+++ Tache indépendates/Tache1.m
@@ -1,11 +1,11 @@
- %__________________________________________________________________%
-%_______________Projet de Communication Numérique ________________%
+%__________________________________________________________________%
+%_______________Projet de Communication NumÃ©rique ________________%
 %__________________________________________________________________%
 %____________Alexandra Abulaeva & Julien Choveton-Caillat__________%
 
 clear ;close all ; clc
 
-addpath('../src/Client', '../src/General', '../src/MAC', '../src/PHY'); % Ajout d'emplacement de certains scripts/fonctions
+
 %%
 %%%%%%%%%%%%%%%%%%%  Variables   %%%%%%%%%%%%%%%%%%%%
 
@@ -14,27 +14,26 @@ TEB_activation = true;
 Affichage_courbe = true;
 
 %%%%%%%%%%%%%%%%%%%  Initialisation des variables  %%%%%%%%%%%%%%%%%%%%
-%Variables général
+%Variables gÃ©nÃ©ral
 
 M = 2;                                      % Nombre de symboles
-Fe = 4e6;                                 % fréquence d'échantillonage
-Te = 1/Fe;                                  % Période d'échantillonage
+Fe = 2*1e7;                                 % frÃ©quence d'Ã©chantillonage
+Te = 1/Fe;                                  % PÃ©riode d'Ã©chantillonage
 Ts = 1e-6;                                  % Temps Symbole
 Ds = 1/Ts;                                  % Debit symbole
-Fse = Ts*Fe;                                % Facteur de sur-échantillonnage
-Ns = 112;
-%Ns=5;% Nombre de symbole/bits par messages
-Nb = Ns;                                    % Nombre de bits par messages on a �galit� cas particulier d'une 2 PPM
+Fse = Ts*Fe;                                % Facteur de sur-Ã©chantillonnage
+Ns = 1000;                                  % Nombre de symbole/bits par messages
+Nb = Ns;                                    % Nombre de bits par messages on a ï¿œgalitï¿œ cas particulier d'une 2 PPM
 ecart_type = 0;
 compteur_erreur = 0;
 
 
 % Variables pour le TEB
 
-Eb = 5;                                      %10 % a trouver l'erreur binaire th�orique donner par Yassine
-eb_sur_No_db = 0:1:10;                       % cr�ation du vecteur qui contient les vecteur espac� de 0.5 pour le trac� de la TEB
-eb_sur_no_dc = 10.^(eb_sur_No_db./10);       % Calcule du vecteur qui contient les vecteurs espac� en decimale pour la TEB
-sigma = sqrt((1/2)*(1./eb_sur_no_dc)*Eb);    %NO/2 % vecteur qui r�pr�sente l'ensemble des sigma a injecter dans la teb
+Eb = 5;                                      %10 % a trouver l'erreur binaire thï¿œorique donner par Yassine
+eb_sur_No_db = 0:1:10;                       % crï¿œation du vecteur qui contient les vecteur espacï¿œ de 0.5 pour le tracï¿œ de la TEB
+eb_sur_no_dc = 10.^(eb_sur_No_db./10);       % Calcule du vecteur qui contient les vecteurs espacï¿œ en decimale pour la TEB
+sigma = sqrt((1/2)*(1./eb_sur_no_dc)*Eb);    %NO/2 % vecteur qui rï¿œprï¿œsente l'ensemble des sigma a injecter dans la teb
 TEB = zeros(1,size(sigma,2));                % initialisation du vecteur de la teb
 compteur_paquet = 0;                         % compteur necessaire au bon fonctionnement de la teb
 erreur = 0;                                  % seuil d'erreur pour arrete un calcule le teb
@@ -55,13 +54,13 @@ p_inverse = fliplr(p);
 %%
 %%%%%%%%%%%%%%%%%%%%%%%% Canal %%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-%________________G�n�ration du signal � envoy�_________________
-signal_bits = randi(2,1,Ns)-1;                     % génération du signal a envoyé
+%________________Génération du signal à envoyé_________________
+signal_bits = randi(2,1,Ns)-1;                     % gÃ©nÃ©ration du signal a envoyÃ©
 %signal_bits = [1 0 0 1 0];
 
 
 %_________________ Association Bits/symboles___________________
-sl=[];                           % cette partie du code et r�impl�ment� dans modulateppm
+sl=[];                           % cette partie du code et réimplémenté dans modulateppm
 for i=1:length(signal_bits)
     if signal_bits(i) == 0       
         sl=[sl po];       
@@ -71,7 +70,7 @@ for i=1:length(signal_bits)
 end
 
 
-% ________________ Cr�ation et ajout du bruit__________________
+% ________________ Création et ajout du bruit__________________
 
 nl = ecart_type * randn(1, length(sl));
 yl=sl+nl;
@@ -81,7 +80,7 @@ yl=sl+nl;
 % _________________Filtre de reception_______________________
 
 
-rl = conv(yl, p_inverse);                          % pour mettre le signal dans la même base il y a deux période car l'un est échantilloné a Ts et l'autre a Te
+rl = conv(yl, p_inverse);                          % pour mettre le signal dans la mÃªme base il y a deux pÃ©riode car l'un est Ã©chantillonÃ© a Ts et l'autre a Te
 rm = rl(Fse:Fse:Fse*Ns);                           % a vori ce que cela fait je me rapelle plus
 
 %_________________ Filtre de decision_________________________
@@ -111,24 +110,23 @@ tic
 if TEB_activation == true
      signal_bits_teb = randi(2,1,Ns)-1; 
          
-     sl = modulatePPM(signal_bits_teb,Fse); % Association Bits/symboles__
-     % génération du signal a envoyé
+     sl = modulatePPM(signal_bits_teb, Fse); % Association Bits/symboles__
+     % gÃ©nÃ©ration du signal a envoyÃ©
      
      for k=1:size(sigma,2) % pour chanque valeur de sigma nous allons calculer un teb
 
 
             while erreur < 100                           % si le nombre d'erreur depasse 100 on sort de la boucle
-                                                         % dans cette partie nous repetons le nombre de fois la partie pr�c�dente
+                                                         % dans cette partie nous repetons le nombre de fois la partie prï¿œcï¿œdente
                 compteur_paquet = compteur_paquet+1;
                 
-                %g�n�ration du bruit du signal
+                %gï¿œnï¿œration du bruit du signal
                 
-                nl = sigma(k) * randn(1, length(sl));
                 yl=sl+nl;
-                
+                nl = sigma(k) * randn(1, length(sl));
                 
                 % demodulation et calcul du nombre d'erreurs
-                [signal_recu_teb] = demodulatePPM(yl,Fse);
+                signal_recu_teb = demodulatePPM(yl,Fse);
                 nb_erreur = sum(ne(signal_recu_teb,signal_bits_teb));
                  
                  %nb_erreur = 0;
@@ -147,15 +145,15 @@ if TEB_activation == true
      end
 end
 
-pb = 1/2*erfc(sqrt(eb_sur_no_dc));  % probabilit� d'erreur th�orique
+pb = 1/2*erfc(sqrt(eb_sur_no_dc));  % probabilité d'erreur théorique
 
 toc
 %%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%  Figures de résultats  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%  Figures de rÃ©sultats  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 if Affichage_courbe==true
 
-    % Représentation de sl(t)
+    % ReprÃ©sentation de sl(t)
     
     figure;
     plot(linspace(0,50*Ts-Te,length(sl)),sl,'linewidth',2);
@@ -163,7 +161,7 @@ if Affichage_courbe==true
     xlabel("t en secondes");
     ylabel("sl(t) en amplitude symbol");
 
-    % Représentation de rl(t)
+    % ReprÃ©sentation de rl(t)
 
     figure;
     plot(linspace(0,50*Ts-Te,length(rl)),rl,'g','linewidth',2);
@@ -171,7 +169,7 @@ if Affichage_courbe==true
     xlabel("t en secondes");
     ylabel("Amplitude symbol");
    
-    % Représentation de rm(t)
+    % ReprÃ©sentation de rm(t)
     
     figure;
     plot(linspace(0,50*Ts-Te,length(rm)),rm,'r','linewidth',2);
@@ -192,10 +190,3 @@ if Affichage_courbe==true
     set(gca,'FontSize',14);
    end
 end
-
-
-%commentaire
-%Energie du filtre pas calculer de mani�re automatique � faire  Eb est d�finie a la main. 
-%Choix du type de message d�finie ou al�atoire en d�but de script mettre
-%variable qui permet de choisir
-% pb depend de la constellation et du nombre de signal
